e2c69412919a7a3edd859e47825e0edb1a835df5
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 02:32:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,6 +12,14 @@ class Triangle
     if @sides.max >= @sides.min(2).sum
       raise TriangleError
     else
+      if @sides.uniq.length == 1
+        :equilateral
+      elsif @sides.uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    end
   end
 
 
