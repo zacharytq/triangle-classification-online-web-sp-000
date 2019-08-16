ce9f0df47911151cc05cdd6ddad07549437dc5b6
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 02:34:22 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
   # write code here
+  def initialize(aa, bb, cc)
+
+    @sides = [aa, bb, cc]
+  end
+
+  class TriangleError <StandardError
+  end
+
+  def kind
+    if @sides.max >= @sides.min(2).sum
+      raise TriangleError
+    else
+      if @sides.uniq.length == 1
+        :equilateral
+      elsif @sides.uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+
 end
