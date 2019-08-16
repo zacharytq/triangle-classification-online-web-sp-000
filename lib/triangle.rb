5b7c09e8746f9ff306e13cb1dbef7638661257f5
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 02:07:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,9 +1,14 @@
 class Triangle
   # write code here
+  def initialize(aa, bb, cc)
+    @aa = aa
+    @bb = bb
+    @cc = cc
+  end
   class TriangleError <StandardError
   end
 
-  def kind(a, b, c)
+  def kind
     if a == b && a == c
       :equilateral
     end
