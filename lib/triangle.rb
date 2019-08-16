89916b1f1ab5441cc0a7f10d4c97bb8228c56583
chic-page-6502 <zacharytq@gmail.com>
2019-08-16 02:11:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,19 +1,17 @@
 class Triangle
   # write code here
   def initialize(aa, bb, cc)
-    @aa = aa
-    @bb = bb
-    @cc = cc
-    @sides = [@aa, @bb, @cc]
+
+    @sides = [aa, bb, cc]
   end
 
   class TriangleError <StandardError
   end
 
   def kind
-    if @aa == @bb && @aa == @cc
+    if @sides.uniq.length == 1
       :equilateral
-    elsif @aa ==
+    elsif
   end
 
 
